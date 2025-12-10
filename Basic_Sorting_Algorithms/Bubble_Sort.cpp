1f6080ac1290d2bdea799f4e024cede639aac89d
Sh-Ifti <shahariarifti16@gmail.com>
2025-12-10 17:10:16 +0600

    Added Bubble Sort

---
+++ Basic_Sorting_Algorithms/Bubble_Sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+}
+
+void buubleSort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Before Sort: ";
+    printArray(arr, n);
+
+    buubleSort(arr, n);
+
+    cout << "\nAfter Sort: ";
+    printArray(arr, n);
+
+    return 0;
+}
+
+// Enter the size of the array: 5
+// Enter the elements: 
+// 5
+// 4
+// 3
+// 2
+// 1
+// Before Sort: 5, 4, 3, 2, 1, 
+// After Sort: 1, 2, 3, 4, 5, 
