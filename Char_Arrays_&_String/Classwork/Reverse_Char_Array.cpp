fcd23c006226fe4db4b9ae9465f03ebdeebc65a0
Sh-Ifti <shahariarifti16@gmail.com>
2025-12-19 02:44:52 +0600

    Added Reverse a character array

---
+++ Char_Arrays_&_String/Classwork/Reverse_Char_Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void reverse(char ch[], int n)
+{
+    int st = 0, end = n - 1;
+
+    while (st < end)
+    {
+        swap(ch[st], ch[end]);
+        st++;
+        end--;
+    }
+}
+
+int main()
+{
+    char word[] = "code";
+
+    cout << "Before Reverse: " << word;
+
+    reverse(word, strlen(word));
+
+    cout << "\nAfter Reverse: " << word;
+
+    return 0;
+}
+
+// Before Reverse: code
+// After Reverse: edoc
