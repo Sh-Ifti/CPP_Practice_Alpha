11c44f359066c1bc523e7a13e7e330317e1fa1a1
Sh-Ifti <shahariarifti16@gmail.com>
2025-12-21 01:35:53 +0600

    Added String basics

---
+++ Char_Arrays_&_String/Classwork/String_Start.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << str[0] << endl;
+    cout << str[1] << endl;
+    cout << str[2] << endl;
+    cout << str << endl;
+
+    return 0;
+}
+
+// hello world
+// h
+// e
+// l
+// hello world
